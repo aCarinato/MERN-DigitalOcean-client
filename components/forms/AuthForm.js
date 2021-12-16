2abb82828cef5b22c4c31d9aa9b65f0f9bfebdf5
Alessandro Carinato <alessandrocarinato@Alessandros-MacBook-Pro.local>
2021-12-16 15:51:52 +0100

    reload image

--- components/forms/AuthForm.js
+++ components/forms/AuthForm.js
@@ -1,6 +1,6 @@
 import { SyncOutlined } from '@ant-design/icons';
 
-export default function AuthForm({
+const AuthForm = ({
   handleSubmit,
   name,
   setName,
@@ -12,69 +12,102 @@ export default function AuthForm({
   setSecret,
   loading,
   page,
-}) {
-  return (
-    <form onSubmit={handleSubmit}>
-      {page !== 'login' && (
-        <div className="form-group p-2">
-          <small>
-            <label className="text-muted">Your Name</label>
-          </small>
-          <input
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            type="text"
-            className="form-control"
-            placeholder="Enter Name"
-          ></input>
-        </div>
-      )}
-
+  username,
+  setUsername,
+  about,
+  setAbout,
+  profileUpdate,
+}) => (
+  <form onSubmit={handleSubmit}>
+    {profileUpdate && (
       <div className="form-group p-2">
         <small>
-          <label className="text-muted">Email Address</label>
+          <label className="text-muted">Username</label>
         </small>
         <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          type="text"
           className="form-control"
-          placeholder="Enter Name"
-        ></input>
+          placeholder="Enter name"
+        />
       </div>
+    )}
 
+    {profileUpdate && (
       <div className="form-group p-2">
         <small>
-          <label className="text-muted">Password</label>
+          <label className="text-muted">About</label>
         </small>
         <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
+          value={about}
+          onChange={(e) => setAbout(e.target.value)}
+          type="text"
           className="form-control"
-          placeholder="Enter Password"
-        ></input>
+          placeholder="Write about yourself.."
+        />
       </div>
+    )}
 
-      {page !== 'login' && (
+    {page !== 'login' && (
+      <div className="form-group p-2">
+        <small>
+          <label className="text-muted">Your name</label>
+        </small>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          className="form-control"
+          placeholder="Enter name"
+        />
+      </div>
+    )}
+
+    <div className="form-group p-2">
+      <small>
+        <label className="text-muted">Email address</label>
+      </small>
+      <input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        type="email"
+        className="form-control"
+        placeholder="Enter name"
+        disabled={profileUpdate}
+      />
+    </div>
+
+    <div className="form-group p-2">
+      <small>
+        <label className="text-muted">Password</label>
+      </small>
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+        className="form-control"
+        placeholder="Enter password"
+      />
+    </div>
+
+    {page !== 'login' && (
+      <>
         <div className="form-group p-2">
           <small>
             <label className="text-muted">Pick a question</label>
           </small>
           <select className="form-control">
             <option>What is your favourite color?</option>
-            <option>What is your best friend name?</option>
-            <option>What city were you born in?</option>
+            <option>What is your best friend's name?</option>
+            <option>What city you were born?</option>
           </select>
-          <small>
-            <label className="form-text text-muted">
-              You can use this to reset your password
-            </label>
+
+          <small className="form-text text-muted">
+            You can use this to reset your password if forgotten.
           </small>
         </div>
-      )}
 
-      {page !== 'login' && (
         <div className="form-group p-2">
           <input
             value={secret}
@@ -82,22 +115,26 @@ export default function AuthForm({
             type="text"
             className="form-control"
             placeholder="Write your answer here"
-          ></input>
+          />
         </div>
-      )}
+      </>
+    )}
 
-      <div className="form-group p-2">
-        <button
-          disabled={
-            page === 'login'
-              ? !email || !password || loading
-              : !name || !email || !password || !secret || loading
-          }
-          className="btn btn-primary col-12"
-        >
-          {loading ? <SyncOutlined spin className="py-1" /> : 'Submit'}
-        </button>
-      </div>
-    </form>
-  );
-}
+    <div className="form-group p-2">
+      <button
+        disabled={
+          profileUpdate
+            ? loading
+            : page === 'login'
+            ? !email || !password || loading
+            : !name || !email || !secret || !password || loading
+        }
+        className="btn btn-primary col-12"
+      >
+        {loading ? <SyncOutlined spin className="py-1" /> : 'Submit'}
+      </button>
+    </div>
+  </form>
+);
+
+export default AuthForm;
